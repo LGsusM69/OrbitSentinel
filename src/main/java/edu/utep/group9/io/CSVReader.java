f5f22d4737cb27afe77594151d3bc2ea36f47bc2
LGsusM69 <jissousklp@gmail.com>
2025-04-19 23:23:23 -0600

    comment

--- src/main/java/edu/utep/group9/io/CSVReader.java
+++ src/main/java/edu/utep/group9/io/CSVReader.java
@@ -3,6 +3,16 @@ package edu.utep.group9.io;
 import java.io.*;
 
 public class CSVReader {
+    
+    
+    /* karl create objects like this:
+    SpaceObject object = new DebrisBuilder()
+    .recordID(1234)
+    .noradCatID(bla bla)
+    .satelliteName("bla bla bla")
+    .....
+
+     */
     private File file;
     private BufferedReader reader;
 
