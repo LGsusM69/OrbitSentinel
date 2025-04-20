4da6f1c6bf10ad16cdfb603e78090ad7e4d787ce
Karl <karl.christian.elvio.prasuhn@gmail.com>
2025-04-19 22:49:05 -0600

    User class

--- src/main/java/edu/utep/group9/models/User.java
+++ src/main/java/edu/utep/group9/models/User.java
@@ -1,4 +1,49 @@
 package edu.utep.group9.models;
 
-public abstract class User {
+public class User {
+    private String username;
+    private String password; // In a real system, use hashed passwords
+    private Role role;
+
+    public enum Role {
+        SCIENTIST,
+        SPACE_AGENCY_REPRESENTATIVE,
+        POLICYMAKER,
+        ADMINISTRATOR
+    }
+
+    public User(String username, String password, Role role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    // Getters
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    // Setters
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+
 }
+
